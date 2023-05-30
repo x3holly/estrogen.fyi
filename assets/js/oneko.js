ce505c1c686bb8ea7579989aff86e183ba15e049
x3holly <134257287+x3holly@users.noreply.github.com>
2023-05-30 04:27:56 -0700

    jekyll my love

--- assets/js/oneko.js
+++ assets/js/oneko.js
@@ -86,7 +86,7 @@ function getRandomInt(min, max) {
       nekoEl.style.height = "32px";
       nekoEl.style.position = "fixed";
       nekoEl.style.pointerEvents = "none";
-      nekoEl.style.backgroundImage = "url('/img/maia_oneko.gif')";
+      nekoEl.style.backgroundImage = "url('/assets/images/oneko.gif')";
       nekoEl.style.imageRendering = "pixelated";
       nekoEl.style.left = `${nekoPosX}px`;
       nekoEl.style.top = `${nekoPosY}px`;
